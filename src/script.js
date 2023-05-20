e1021966047798836de5e29b1f7102d7dcacac6c
thomascarre <carrethomas7@gmail.com>
2023-05-20 16:26:51 +0200

    changement matcap vers standardmesh

--- src/script.js
+++ src/script.js
@@ -1,77 +1,77 @@
+// Importation des modules
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
+// Configuration de base
+const gui = new dat.GUI() // Interface de débogage
+const canvas = document.querySelector('canvas.webgl') // Sélectionne le canvas WebGL
+const scene = new THREE.Scene() // Crée une nouvelle scène
 
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI()
+// Chargement des textures
+const textureLoader = new THREE.TextureLoader() // Chargeur de textures
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+const environmentMapTexture = new THREE.CubeTextureLoader().load([
+  '/textures/environmentMaps/3/px.jpg',
+  '/textures/environmentMaps/3/nx.jpg',
+  '/textures/environmentMaps/3/py.jpg',
+  '/textures/environmentMaps/3/ny.jpg',
+  '/textures/environmentMaps/3/pz.jpg',
+  '/textures/environmentMaps/3/nz.jpg'
+]) // Texture de l'environnement
 
-// Scene
-const scene = new THREE.Scene()
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg') // Texture de metalness
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg') // Texture de roughness
 
-//AxesHelper
-// const axesHelper = new THREE.AxesHelper()
-// scene.add(axesHelper)
+// Chargement des polices
+const fontLoader = new FontLoader() // Chargeur de polices
 
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
+fontLoader.load('/fonts/Bruno Ace SC_Regular.json', (font) => {
+  // Géométrie du texte
+  const textGeometry = new TextGeometry('STUDIO CARRE', {
+    font: font,
+    size: 0.7,
+    height: 0.6,
+    curveSegments: 6,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4
+  })
 
-const matCapTexture = textureLoader.load('/textures/matcaps/8.png')
+  textGeometry.center()
 
+  // Matériau du texte
+  const material = new THREE.MeshStandardMaterial()
 
-//Fonts
-const fontLoader = new FontLoader()
-fontLoader.load(
-  '/fonts/helvetiker_regular.typeface.json',
-  (font) =>
-  {
-    const textGeometry = new TextGeometry(
-      'STUDIO CARRE',
-      {
-        font: font,
-        size: 0.5,
-        height: 0.2,
-        curveSegments: 6,
-        bevelEnabled: true,
-        bevelThickness: 0.03,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 4
-      }
-    )
+  material.metalness = 0.8
+  material.roughness = 0.05
+  material.aoMapIntensity = 1
+  material.envMap = environmentMapTexture
+  material.displacementScale = 1
 
-    textGeometry.center()
+  // Ajout des propriétés au panneau de configuration
+  gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+  gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+  gui.add(material, 'transparent').min(0).max(1).step(0.0001)
 
-    const material = new THREE.MeshMatcapMaterial()
-    material.matcap = matCapTexture
-    const text = new THREE.Mesh(textGeometry, material)
-    scene.add(text)
+  // Création du maillage pour le texte
+  const text = new THREE.Mesh(textGeometry, material)
+  scene.add(text)
 
+  // Géométrie du donut (boîte)
+  const donutGeometry = new THREE.BoxGeometry(1, 1, 1, 1, 20, 45)
 
-    console.time('donuts');
-
-
-    const donutGeometry = new THREE.TorusGeometry(0.4, 0.2, 20, 45)
-
-
-    for(let i = 0; i < 1000; i++)
-{
+  // Création des maillages de donuts
+  for (let i = 0; i < 100; i++) {
     const donut = new THREE.Mesh(donutGeometry, material)
 
     donut.position.x = (Math.random() - 0.5) * 10
     donut.position.y = (Math.random() - 0.5) * 10
     donut.position.z = (Math.random() - 0.5) * 10
-
 
     donut.rotation.x = Math.random() * Math.PI
     donut.rotation.y = Math.random() * Math.PI
@@ -81,94 +81,66 @@ fontLoader.load(
     donut.scale.x = scale
     donut.scale.y = scale
     donut.scale.z = scale
+
     scene.add(donut)
-}
-console.timeEnd('donuts')
-
-
-    // textGeometry.computeBoundingBox()
-  //   textGeometry.translate(
-  //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-  //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-  //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-  //   )
-}
-)
-
-
-/**
- * Object
- */
-// const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial()
-// )
-
-// scene.add(cube)
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  }
 })
 
-/**
- * Camera
- */
-// Base camera
+// Tailles de la fenêtre
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  // Mise à jour des tailles
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Mise à jour de la caméra
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Mise à jour du rendu
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Caméra
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
+// Contrôles de la caméra
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Rendu
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+// Boucle d'animation
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Mise à jour des objets
 
-    // Render
-    renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Mise à jour des contrôles
+  controls.update()
+
+  // Rendu
+  renderer.render(scene, camera)
+
+  // Appel de tick à la prochaine frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
